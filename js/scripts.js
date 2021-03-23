f3e53b53a8d6a1dfbdc7271b3bcdf622f6f9d625
Thomas Russell <t.p.russell19@gmail.com>
2021-03-23 14:35:20 -0700

    create a page with buttons to change background color
    Co-authored-by: Marney Mallory <marney.mallory@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#original").click(function() {
+    $("body").removeClass();
+  });
+});
